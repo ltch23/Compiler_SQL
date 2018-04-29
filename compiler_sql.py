88bfd48b0ce8c295056dbc4c4d3db6d35fa16c89
ltch23 <luistong23@gmail.com>
2018-04-29 00:31:18 -0500

    sintactic

--- compiler_sql.py
+++ compiler_sql.py
@@ -28,7 +28,7 @@ def preprocesing():
         uncommetns = remove_comments(renglon)
         uncommetns = uncommetns.split(' ')
         for word in  uncommetns:
-            separators= r"([+]|-|[*]|;|,|=|<=|>=|<|>|[(]|[)]|[[]|[]]|{|})"
+            separators= r"([+]|-|[*]|;|,|<=|>=|<|>|=|\n|[(]|[)]|[[]|[]]|{|})"
             splits= re.split(separators, word)
             list_splits = filter(None,splits)
             for lexem in list_splits:
@@ -49,17 +49,16 @@ tkn_num_int = re.compile('[0-9]+')
 tkn_num_float = re.compile('[0-9]+.[0-9]+')
 tkn_num_date = re.compile('\"[0-9]+\/[0-9]+\/[0-9]+\"')
 tkn_num_time= re.compile('\"[0-9]+:[0-9]+:[0-9]+\"')
-tkn_string = re.compile('\"[a-zA-Z]+[a-zA-Z1-9_]*\"')
+tkn_varchar = re.compile('\"[a-zA-Z]+[a-zA-Z1-9_]*\"')
 
-tkn_list = [["int","tkn_int"],["varchar","tkn_varchar"],["date","tkn_date"],["time","tkn_time"],
-            ["select","tkn_select"],["insert","tkn_insert"],["update","tkn_update"],["delete","tkn_delete"],
-            ["from","tkn_from"],["where","tkn_where"],["all","tkn_all"],["values","tkn_values"],
-            ["(","("],[")",")"],[";",";"],[",",","],
-            ["=","="],["<","<"],[">",">"],["<=","<="],["==","=="],["!=","!="],
+tkn_list = [["select","tkn_select"],["insert","tkn_insert"],["update","tkn_update"],["delete","tkn_delete"],
+            ["from","tkn_from"],["where","tkn_where"],["*","tkn_*"], ["avg","tkn_avg"],["sum","tkn_sum"],["count","tkn_count"],
+            ["group_by","tkn_group_by"],["inner_join","tkn_inner_join"],["on","tkn_on"],["values","tkn_values"],["set","tkn_set"],
+            ["(","tkn_("],[")","tkn_)"],[";",";"],[",","tkn_,"],
+            ["=","tkn_="],["<","tkn_<"],[">","tkn_>"],["<=","tkn_<="],[">=","tkn_>="],
             ["and","tkn_and"],["or","tkn_or"],["not","tkn_not"],
-            ["+","+"],["-","-"],["*","*"],["/","/"],
             ]
-
+            
 
 def tokens():
 
@@ -76,41 +75,47 @@ def tokens():
                 if len(m.group(0)) == len(list_token[token][0]):
                     list_token[token][1] = "tkn_id"
                 else:
-                    print ("Error string not found in the line: ", list_token[token][0])
+                    print ("Error string not found in the line: ", list_token[token][2])
             
+            elif re.match(tkn_varchar, list_token[token][0]):
+                m = re.match(tkn_varchar, list_token[token][0])
+                if len(m.group(0)) == len(list_token[token][0]):
+                    list_token[token][1] = "tkn_varchar"
+                else:
+                    print ("Error string not found in the line: ", list_token[token][2])
             elif re.match(tkn_num_date, list_token[token][0]):
                 m = re.match(tkn_num_date, list_token[token][0])
                 if len(m.group(0)) == len(list_token[token][0]):
-                    list_token[token][1] = "tkn_num_date"
+                    list_token[token][1] = "tkn_date"
                 else:
-                    print ("Error string not found in the line: ", list_token[token][0])
+                    print ("Error string not found in the line: ", list_token[token][2])
             elif re.match(tkn_num_time, list_token[token][0]):
                 m = re.match(tkn_num_time, list_token[token][0])
                 if len(m.group(0)) == len(list_token[token][0]):
-                    list_token[token][1] = "tkn_num_time"
+                    list_token[token][1] = "tkn_time"
                 else:
-                    print ("Error string not found in the line: ", list_token[token][0])
-            # if re.match(tkn_string, list_token[token][0]):
+                    print ("Error string not found in the line: ", list_token[token][2])
+            # if re.match(tkn_varchar, list_token[token][0]):
             elif re.match(tkn_num_float, list_token[token][0]):
                 m = re.match(tkn_num_float, list_token[token][0])
                 if len(m.group(0)) == len(list_token[token][0]):
-                    list_token[token][1] = "tkn_num_float"
+                    list_token[token][1] = "tkn_float"
                 else:
-                    print ("Error string not found in the line: ", list_token[token][0])
-            #     m = re.match(tkn_string, list_token[token][0])
+                    print ("Error string not found in the line: ", list_token[token][2])
+            #     m = re.match(tkn_varchar, list_token[token][0])
             elif re.match(tkn_num_int, list_token[token][0]):
                 m = re.match(tkn_num_int, list_token[token][0])
                 if len(m.group(0)) == len(list_token[token][0]):
-                    list_token[token][1] = "tkn_num_int"
+                    list_token[token][1] = "tkn_int"
                 else:
-                    print ("Error string not found in the line: ", list_token[token][0])
+                    print ("Error string not found in the line: ", list_token[token][2])
             
-            elif re.match(tkn_string, list_token[token][0]):
-                m = re.match(tkn_string, list_token[token][0])
-                if len(m.group(0)) == len(list_token[token][0]):
-                    list_token[token][1] = "tkn_string"
-                else:
-                    print ("Error string not found in the line: ", list_token[token][0])
+
+            if list_token[token][1]=="tkn_undefined":
+                print("print ",[list_token[token][0],list_token[token][1],list_token[token][2]])
+                list_token[token].pop()
+                list_token[token].pop()
+                list_token[token].pop()
             #     #bitacora
 #***************************************************************************************
 
@@ -118,23 +123,12 @@ def tokens():
 table_sim = {}
 
 def tabla_sim():
-    for id in range(0,len(list_token)):
-        if list_token[id][1] == "tkn_id":
-            if list_token[id+1][1] == "=":
-                if list_token[id+2][1]=="tkn_num" or list_token[id+2][1]=="tkn_varchar"  :
-                    
-                    if not list_token[id][0] in table_sim:
-                        table_sim[list_token[id][0]] ={'Lexem': list_token[id][1],'Value': list_token[id+2][0],
-                        'Len': len(list_token[id][0]), 'Data_type':list_token[id+2][1],'Line': list_token[id][2]}
-                    else:
-                        table_sim[list_token[id][0]]['Line'].append(list_token[id][2])
-            
-            else:
-                if not list_token[id][0] in table_sim:
-                    table_sim[list_token[id][0]] ={'Lexem':     list_token[id][1], 'Value': '',
-                    'Len': len(list_token[id][0]), 'Data_type':'tkn_varchar','Line': list_token[id][2]}
-                else:
-                    table_sim[list_token[id][0]]['Line'].append(list_token[id][2])
+    for t in range(0,len(list_token)):
+        if list_token[t][1] == "tkn_id" and list_token[t]:
+            if list_token[t][0] not in table_sim:
+                 table_sim[list_token[t][0]]= {'Lexem':list_token[t][1], 'Value': '','Len': len(list_token[t][0]), 'Data_type':'','Line': list_token[t][2]}
+            # if  list_token[id][0] in table_sim:
+                 # table_sim[list_token[id][0]]['Line'].append(list_token[id][2])
 
 #***************************************************************************************
 
@@ -145,8 +139,153 @@ def print_TS():
 
 
 
+terminales = {
+'tkn_select':1,'tkn_insert':2,'tkn_update':3,'tkn_delete':4,'tkn_avg':5,'tkn_sum':6,'tkn_count':7,'tkn_from':8,'tkn_inner_join':9,'tkn_on':10,'tkn_where':11,'tkn_group_by':12,'tkn_order_by':13,'tkn_values':14,'tkn_set':15,'tkn_*':16,'tkn_id':17,'tkn_.':18,'tkn_(':19,'tkn_)':20,'tk_and':21,'tkn_or':22,'tkn_<':23,'tkn_>':24,'tkn_<=':25,'tkn_>=':26,'tkn_=':27,'tkn_,':28,'tkn_int':29,'tkn_float':30,'tkn_time':31,'tkn_date':32,'tkn_varchar':33,'$':34,
+}
+
+
+noTerminales = {
+'A':1,'S':2,'F':3,'J':4,'W':5,'G':6,'I':7,'U':8,'op_rel':9,'op_com':10,'numeros':11,'valor_s':12,'valor_c':13,'valores':14,'id_tmp':15,'id_sim':16,'id_com':17,'ids':18,'C':19,'id_val':20,'cond_sim':21,'cond_com':22,'conditions':23,'set_sim':24,'set_com':25,'sets':26,
+}
+
+
+
+tablaSintactica = {
+1:['tkn_select', 'S','F','J','W','G'],   2:['tkn_insert_into', 'I'], 3:['tkn_update', 'U','W'], 4:['tkn_delete','F','W'], 5:[''],6:[''],7:[''],8:[''],9:[''],10:[''],11:[''],12:[''],13:[''],14:[''],15:[''],16:[''],17:[''],18:[''],19:[''],20:[''],21:[''],22:[''],23:[''],24:[''],25:[''],26:[''],27:[''],28:[''],29:[''],30:[''],31:[''],32:[''],33:[''],34:[''],
+
+35:[''],36:[''],37:[''],38:[''],39:['C','tkn_(','ids','tkn_)'],40:['C','tkn_(','ids','tkn_)'],41:['C','tkn_(','ids','tkn_)'],42:[''],43:[''],44:[''],45:[''],46:[''],47:[''],48:[''],49:[''],50:['tkn_*'],51:['ids'],52:[''],53:[''],54:[''],55:[''],56:[''],57:[''],58:[''],59:[''],60:[''],61:[''],62:[''],63:[''],64:[''],65:[''],66:[''],67:[''],68:[''],
+
+69:[''],70:[''],71:[''],72:[''],73:[''],74:[''],75:[''],76:['tkn_from','ids'],77:[''],78:[''],79:[''],80:[''],81:[''],82:[''],83:[''],84:[''],85:[''],86:[''],87:[''],88:[''],89:[''],90:[''],91:[''],92:[''],93:[''],94:[''],95:[''],96:[''],97:[''],98:[''],99:[''],100:[''],101:[''],102:[''],
+
+103:[''],104:[''],105:[''],106:[''],107:[''],108:[''],109:[''],110:[''],111:['tkn_inner_join','id_sim','tkn_on','id_sim','tkn_=','id_sim','J'],112:[''],113:['vacio'],114:['vacio'],115:[''],116:[''],117:[''],118:[''],119:[''],120:[''],121:[''],122:[''],123:[''],124:[''],125:[''],126:[''],127:[''],128:[''],129:[''],130:[''],131:[''],132:[''],133:[''],134:[''],135:[''],136:['vacio'],
+
+137:[''],138:[''],139:[''],140:[''],141:[''],142:[''],143:[''],144:[''],145:[''],146:[''],147:['tkn_where','conditions'],148:['vacio'],149:[''],150:[''],151:[''],152:[''],153:[''],154:[''],155:[''],156:[''],157:[''],158:[''],159:[''],160:[''],161:[''],162:[''],163:[''],164:[''],165:[''],166:[''],167:[''],168:[''],169:[''],170:['vacio'],
+
+171:[''],172:[''],173:[''],174:[''],175:[''],176:[''],177:[''],178:[''],179:[''],180:[''],181:[''],182:[''],183:['tkn_group_by','ids','tkn_order_by','ids'],184:[''],185:[''],186:[''],187:[''],188:[''],189:[''],190:[''],191:[''],192:[''],193:[''],194:[''],195:[''],196:[''],197:[''],198:[''],199:[''],200:[''],201:[''],202:[''],203:[''],204:['vacio'],
+
+205:[''],206:[''],207:[''],208:[''],209:[''],210:[''],211:[''],212:[''],213:[''],214:[''],215:[''],216:[''],217:[''],218:[''],219:[''],220:[''],221:['id_sim','tkn_values','tkn_(','valores','tkn_)'],222:[''],223:[''],224:[''],225:[''],226:[''],227:[''],228:[''],229:[''],230:[''],231:[''],232:[''],233:[''],234:[''],235:[''],236:[''],237:[''],238:[''],
+
+239:[''],240:[''],241:[''],242:[''],243:[''],244:[''],245:[''],246:[''],247:[''],248:[''],249:[''],250:[''],251:[''],252:[''],253:[''],254:[''],255:['id_sim','tkn_set','sets'],256:[''],257:[''],258:[''],259:[''],260:[''],261:[''],262:[''],263:[''],264:[''],265:[''],266:[''],267:[''],268:[''],269:[''],270:[''],271:[''],272:[''],
+
+273:[''],274:[''],275:[''],276:[''],277:[''],278:[''],279:[''],280:[''],281:[''],282:[''],283:[''],284:[''],285:[''],286:[''],287:[''],288:[''],289:[''],290:[''],291:[''],292:[''],293:['tkn_and'],294:['tkn_or'],295:[''],296:[''],297:[''],298:[''],299:[''],300:[''],301:[''],302:[''],303:[''],304:[''],305:[''],306:[''],
+
+307:[''],308:[''],309:[''],310:[''],311:[''],312:[''],313:[''],314:[''],315:[''],316:[''],317:[''],318:[''],319:[''],320:[''],321:[''],322:[''],323:[''],324:[''],325:[''],326:[''],327:[''],328:[''],329:['tkn_<'],330:['tkn_>'],331:['tkn_<='],332:['tkn_>='],333:['tkn_='],334:[''],335:[''],336:[''],337:[''],338:[''],339:[''],340:[''],
+
+341:[''],342:[''],343:[''],344:[''],345:[''],346:[''],347:[''],348:[''],349:[''],350:[''],351:[''],352:[''],353:[''],354:[''],355:[''],356:[''],357:[''],358:[''],359:[''],360:[''],361:[''],362:[''],363:[''],364:[''],365:[''],366:[''],367:[''],368:[''],369:['tkn_int'],370:['tkn_float'],371:['tkn_time'],372:['tkn_date'],373:[''],374:[''],
+
+375:[''],376:[''],377:[''],378:[''],379:[''],380:[''],381:[''],382:[''],383:[''],384:[''],385:[''],386:[''],387:[''],388:[''],389:[''],390:[''],391:[''],392:[''],393:[''],394:[''],395:[''],396:[''],397:[''],398:[''],399:[''],400:[''],401:[''],402:[''],403:['numeros'],404:['numeros'],405:['numeros'],406:['numeros'],407:['tkn_varchar'],408:[''],
+
+409:[''],410:[''],411:[''],412:[''],413:[''],414:[''],415:[''],416:[''],417:[''],418:[''],419:[''],420:[''],421:[''],422:[''],423:[''],424:[''],425:[''],426:[''],427:[''],428:['vacio'],429:[''],430:[''],431:[''],432:[''],433:[''],434:[''],435:[''],436:['tkn_,','valor_s','valor_c'],437:[''],438:[''],439:[''],440:[''],441:[''],442:[''],
+
+443:[''],444:[''],445:[''],446:[''],447:[''],448:[''],449:[''],450:[''],451:[''],452:[''],453:[''],454:[''],455:[''],456:[''],457:[''],458:[''],459:[''],460:[''],461:[''],462:[''],463:[''],464:[''],465:[''],466:[''],467:[''],468:[''],469:[''],470:[''],471:['valor_s','valor_c'],472:['valor_s','valor_c'],473:['valor_s','valor_c'],474:['valor_s','valor_c'],475:['valor_s','valor_c'],476:[''],
+
+477:[''],478:[''],479:[''],480:[''],481:[''],482:[''],483:[''],484:['vacio'],485:['vacio'],486:['vacio'],487:['vacio'],488:['vacio'],489:['vacio'],490:['vacio'],491:['vacio'],492:[''],493:[''],494:['tkn_.','tkn_id'],495:[''],496:['vacio'],497:['vacio'],498:['vacio'],499:['vacio'],500:['vacio'],501:['vacio'],502:['vacio'],503:['vacio'],504:['vacio'],505:[''],506:[''],507:[''],508:[''],509:[''],510:['vacio'],
+
+511:[''],512:[''],513:[''],514:[''],515:[''],516:[''],517:[''],518:[''],519:[''],520:[''],521:[''],522:[''],523:[''],524:[''],525:[''],526:[''],527:['tkn_id','id_tmp'],528:[''],529:[''],530:[''],531:[''],532:[''],533:[''],534:[''],535:[''],536:[''],537:[''],538:[''],539:[''],540:[''],541:[''],542:[''],543:[''],544:[''],
+
+545:[''],546:[''],547:[''],548:[''],549:[''],550:[''],551:[''],552:['vacio'],553:['vacio'],554:[''],555:['vacio'],556:['vacio'],557:['vacio'],558:['vacio'],559:[''],560:[''],561:[''],562:[''],563:[''],564:['vacio'],565:[''],566:[''],567:[''],568:[''],569:[''],570:[''],571:[''],572:['tkn_,','id_sim','id_com'],573:[''],574:[''],575:[''],576:[''],577:[''],578:['vacio'],
+
+579:[''],580:[''],581:[''],582:[''],583:[''],584:[''],585:[''],586:[''],587:[''],588:[''],589:[''],590:[''],591:[''],592:[''],593:[''],594:[''],595:['id_sim','id_com'],596:[''],597:[''],598:[''],599:[''],600:[''],601:[''],602:[''],603:[''],604:[''],605:[''],606:[''],607:[''],608:[''],609:[''],610:[''],611:[''],612:['vacio'],
+
+613:[''],614:[''],615:[''],616:[''],617:['avg'],618:['sum'],619:['count'],620:[''],621:[''],622:[''],623:[''],624:[''],625:[''],626:[''],627:[''],628:[''],629:[],630:[''],631:[''],632:[''],633:[''],634:[''],635:[''],636:[''],637:[''],638:[''],639:[''],640:[''],641:[''],642:[''],643:[''],644:[''],645:[''],646:[''],
+
+647:[''],648:[''],649:[''],650:[''],651:[''],652:[''],653:[''],654:[''],655:[''],656:[''],657:[''],658:[''],659:[''],660:[''],661:[''],662:[''],663:['id_sim'],664:[''],665:[''],666:[''],667:[''],668:[''],669:[''],670:[''],671:[''],672:[''],673:[''],674:[''],675:['valor_s'],676:['valor_s'],677:['valor_s'],678:['valor_s'],679:['valor_s'],680:[''],
+
+681:[''],682:[''],683:[''],684:[''],685:[''],686:[''],687:[''],688:[''],689:[''],690:[''],691:[''],692:[''],693:[''],694:[''],695:[''],696:[''],697:['id_sim','op_com','id_val'],698:[''],699:[''],700:[''],701:[''],702:[''],703:[''],704:[''],705:[''],706:[''],707:[''],708:[''],709:[''],710:[''],711:[''],712:[''],713:[''],714:[''],
+
+715:[''],716:[''],717:[''],718:[''],719:[''],720:[''],721:[''],722:[''],723:[''],724:[''],725:[''],726:['vacio'],727:[''],728:[''],729:[''],730:[''],731:[''],732:[''],733:[''],734:[''],735:['op_rel','cond_sim','cond_com'],736:['op_rel','cond_sim','cond_com'],737:[''],738:[''],739:[''],740:[''],741:[''],742:[''],743:[''],744:[''],745:[''],746:[''],747:[''],748:['vacio'],
+
+749:[''],750:[''],751:[''],752:[''],753:[''],754:[''],755:[''],756:[''],757:[''],758:[''],759:[''],760:[''],761:[''],762:[''],763:[''],764:[''],765:['cond_sim','cond_com'],766:[''],767:[''],768:[''],769:[''],770:[''],771:[''],772:[''],773:[''],774:[''],775:[''],776:[''],777:[''],778:[''],779:[''],780:[''],781:[''],782:[''],
+
+783:[''],784:[''],785:[''],786:[''],787:[''],788:[''],789:[''],790:[''],791:[''],792:[''],793:[''],794:[''],795:[''],796:[''],797:[''],798:[''],799:['id_sim','tkn_=','valor_s'],800:[''],801:[''],802:[''],803:[''],804:[''],805:[''],806:[''],807:[''],808:[''],809:[''],810:[''],811:[''],812:[''],813:[''],814:[''],815:[''],816:[''],
+
+817:[''],818:[''],819:[''],820:[''],821:[''],822:[''],823:[''],824:[''],825:[''],826:[''],827:['vacio'],828:[''],829:[''],830:[''],831:[''],832:[''],833:[''],834:[''],835:[''],836:[''],837:[''],838:[''],839:[''],840:[''],841:[''],842:[''],843:[''],844:[''],845:['tkn_,','set_sim','set_com'],846:[''],847:[''],848:[''],849:[''],850:['vacio'],
+
+851:[''],852:[''],853:[''],854:[''],855:[''],856:[''],857:[''],858:[''],859:[''],860:[''],861:[''],862:[''],863:[''],864:[''],865:[''],866:[''],867:['set_sim','set_com'],868:[''],869:[''],870:[''],871:[''],872:[''],873:[''],874:[''],875:[''],876:[''],877:[''],878:[''],879:[''],880:[''],881:[''],882:[''],883:[''],884:[''],
+}
+
+
+
+
+
+
+
+
+
+
+
 preprocesing()
 tokens()
 print_lt()
-# tabla_sim()
-# print_TS()
+
+tabla_sim()
+print_TS()
+
+# print(tablaSintactica)
+pila=['A','$']
+entrada = []
+lexema = []  #Contendrá los lexemnas de los tkn_Identificadores para ubicarlos en la TS
+
+
+
+for x in range(0, len(list_token)):
+    entrada.append(list_token[x][1])
+    lexema.append(list_token[x][0])
+
+entrada.append('$')
+lexema.append('$')
+
+
+print ('**************************************')
+print ("entrada ",entrada)
+print ("lexema ",lexema)
+print ("pila ",pila)
+
+
+
+def tablaSintac():
+    while(len(entrada)>0):
+        print ("Pila:", pila[::-1])
+        print ("Entrada:", entrada)
+        
+        if pila[0] in terminales:
+            if (entrada[0] == pila[0]):
+                print("- ",entrada[0],pila[0])
+                if entrada[0] == '$' and pila[0] == '$':
+                    print("Cadena aceptada sintacticamente")
+                pila.pop(0)
+                entrada.pop(0)
+                lexema.pop(0)
+            else:
+                print("Error sintactico: No se esperaba Token", entrada[0])
+                pila.pop(0)
+                entrada.pop(0)
+                lexema.pop(0)
+
+        else:
+            #Si es entero -> es regla Ejécutala (con q params?)
+            if pila[0].isdigit():
+                pila.pop(0)
+            else:
+                # saco y pongo al revez
+                fila = noTerminales[pila[0]] - 1
+
+                columna = terminales[entrada[0]]
+                print("fila ",fila," col ",columna)
+                empila = tablaSintactica[fila * 34 + columna]
+                print ("data", fila * 34 + columna)
+                empilar = empila[::-1]
+                print ('empila: ', empilar)
+                pila.pop(0)
+                for j in range(0,len(empilar)):
+                    if(empilar[j] == ''):
+                        print("Error sintactico: No se encuentra Token en la Tabla Sintactica ",entrada[0])
+                        entrada.pop(0)
+                        lexema.pop(0)
+                    elif(empilar[j] != 'vacio'):
+                        pila.insert(0,empilar[j])
+
+
+
+
+tablaSintac()
